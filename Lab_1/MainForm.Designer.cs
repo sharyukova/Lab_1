6da322c27a79def0e84838505bc7009e4faeba1b
alina <a.sharyukova@mail.ru>
2024-04-14 00:28:32 +0300

    MainForm added

--- Lab_1/MainForm.Designer.cs
+++ Lab_1/MainForm.Designer.cs
@@ -28,13 +28,212 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.returnAllBtn = new System.Windows.Forms.Button();
+            this.sequrityStatus = new System.Windows.Forms.Label();
+            this.specialCharsBox = new System.Windows.Forms.TextBox();
+            this.saveBtn = new System.Windows.Forms.Button();
+            this.loginField = new System.Windows.Forms.TextBox();
+            this.generateBtn = new System.Windows.Forms.Button();
+            this.passwField = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.passwLength = new System.Windows.Forms.NumericUpDown();
+            this.rockyouPassw = new System.Windows.Forms.CheckBox();
+            this.notSimilarSymb = new System.Windows.Forms.CheckBox();
+            this.specialSymb = new System.Windows.Forms.CheckBox();
+            ((System.ComponentModel.ISupportInitialize)(this.passwLength)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // returnAllBtn
+            // 
+            this.returnAllBtn.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.returnAllBtn.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F);
+            this.returnAllBtn.Location = new System.Drawing.Point(393, 151);
+            this.returnAllBtn.Name = "returnAllBtn";
+            this.returnAllBtn.Size = new System.Drawing.Size(165, 28);
+            this.returnAllBtn.TabIndex = 23;
+            this.returnAllBtn.Text = "Вернуть все символы";
+            this.returnAllBtn.UseVisualStyleBackColor = false;
+            this.returnAllBtn.Click += new System.EventHandler(this.returnAllBtn_Click);
+            // 
+            // sequrityStatus
+            // 
+            this.sequrityStatus.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.sequrityStatus.AutoSize = true;
+            this.sequrityStatus.BackColor = System.Drawing.Color.Transparent;
+            this.sequrityStatus.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.sequrityStatus.ForeColor = System.Drawing.Color.IndianRed;
+            this.sequrityStatus.ImageAlign = System.Drawing.ContentAlignment.BottomLeft;
+            this.sequrityStatus.Location = new System.Drawing.Point(453, 73);
+            this.sequrityStatus.Name = "sequrityStatus";
+            this.sequrityStatus.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.sequrityStatus.Size = new System.Drawing.Size(112, 18);
+            this.sequrityStatus.TabIndex = 22;
+            this.sequrityStatus.Text = "Ненадежный! х\r\n";
+            this.sequrityStatus.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // specialCharsBox
+            // 
+            this.specialCharsBox.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.specialCharsBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.specialCharsBox.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.specialCharsBox.Location = new System.Drawing.Point(369, 118);
+            this.specialCharsBox.Name = "specialCharsBox";
+            this.specialCharsBox.Size = new System.Drawing.Size(189, 27);
+            this.specialCharsBox.TabIndex = 21;
+            this.specialCharsBox.Text = "{}[]()/\\\'\"`~,:.<>";
+            // 
+            // saveBtn
+            // 
+            this.saveBtn.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.saveBtn.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.saveBtn.Location = new System.Drawing.Point(335, 410);
+            this.saveBtn.Name = "saveBtn";
+            this.saveBtn.Size = new System.Drawing.Size(203, 35);
+            this.saveBtn.TabIndex = 20;
+            this.saveBtn.Text = "Сохранить все\r\n";
+            this.saveBtn.UseVisualStyleBackColor = false;
+            this.saveBtn.Click += new System.EventHandler(this.saveBtn_Click);
+            // 
+            // loginField
+            // 
+            this.loginField.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.loginField.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.loginField.ForeColor = System.Drawing.SystemColors.WindowFrame;
+            this.loginField.Location = new System.Drawing.Point(74, 334);
+            this.loginField.Name = "loginField";
+            this.loginField.Size = new System.Drawing.Size(296, 28);
+            this.loginField.TabIndex = 19;
+            this.loginField.Text = "Логин";
+            this.loginField.Enter += new System.EventHandler(this.loginField_Enter);
+            // 
+            // generateBtn
+            // 
+            this.generateBtn.BackColor = System.Drawing.SystemColors.GradientInactiveCaption;
+            this.generateBtn.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.generateBtn.Location = new System.Drawing.Point(415, 256);
+            this.generateBtn.Name = "generateBtn";
+            this.generateBtn.Size = new System.Drawing.Size(143, 41);
+            this.generateBtn.TabIndex = 18;
+            this.generateBtn.Text = "Генерировать";
+            this.generateBtn.UseVisualStyleBackColor = false;
+            this.generateBtn.Click += new System.EventHandler(this.generateBtn_Click);
+            // 
+            // passwField
+            // 
+            this.passwField.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.passwField.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.passwField.ForeColor = System.Drawing.SystemColors.WindowFrame;
+            this.passwField.Location = new System.Drawing.Point(74, 262);
+            this.passwField.Name = "passwField";
+            this.passwField.Size = new System.Drawing.Size(296, 28);
+            this.passwField.TabIndex = 17;
+            this.passwField.Text = "Пароль";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(70, 73);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(132, 20);
+            this.label1.TabIndex = 16;
+            this.label1.Text = "Длина пароля:";
+            // 
+            // passwLength
+            // 
+            this.passwLength.Location = new System.Drawing.Point(233, 74);
+            this.passwLength.Maximum = new decimal(new int[] {
+            40,
+            0,
+            0,
+            0});
+            this.passwLength.Minimum = new decimal(new int[] {
+            6,
+            0,
+            0,
+            0});
+            this.passwLength.Name = "passwLength";
+            this.passwLength.Size = new System.Drawing.Size(120, 22);
+            this.passwLength.TabIndex = 15;
+            this.passwLength.Value = new decimal(new int[] {
+            6,
+            0,
+            0,
+            0});
+            this.passwLength.ValueChanged += new System.EventHandler(this.passwLength_ValueChanged);
+            // 
+            // rockyouPassw
+            // 
+            this.rockyouPassw.AutoSize = true;
+            this.rockyouPassw.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.rockyouPassw.Location = new System.Drawing.Point(74, 181);
+            this.rockyouPassw.Name = "rockyouPassw";
+            this.rockyouPassw.Size = new System.Drawing.Size(279, 24);
+            this.rockyouPassw.TabIndex = 14;
+            this.rockyouPassw.Text = "Нераспространенный пароль";
+            this.rockyouPassw.UseVisualStyleBackColor = true;
+            // 
+            // notSimilarSymb
+            // 
+            this.notSimilarSymb.AutoSize = true;
+            this.notSimilarSymb.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.notSimilarSymb.Location = new System.Drawing.Point(74, 151);
+            this.notSimilarSymb.Name = "notSimilarSymb";
+            this.notSimilarSymb.Size = new System.Drawing.Size(202, 24);
+            this.notSimilarSymb.TabIndex = 13;
+            this.notSimilarSymb.Text = "Непохожие символы";
+            this.notSimilarSymb.UseVisualStyleBackColor = true;
+            // 
+            // specialSymb
+            // 
+            this.specialSymb.AutoSize = true;
+            this.specialSymb.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.specialSymb.Location = new System.Drawing.Point(74, 121);
+            this.specialSymb.Name = "specialSymb";
+            this.specialSymb.Size = new System.Drawing.Size(221, 24);
+            this.specialSymb.TabIndex = 12;
+            this.specialSymb.Text = "Небуквенные символы";
+            this.specialSymb.UseVisualStyleBackColor = true;
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(636, 519);
+            this.Controls.Add(this.returnAllBtn);
+            this.Controls.Add(this.sequrityStatus);
+            this.Controls.Add(this.specialCharsBox);
+            this.Controls.Add(this.saveBtn);
+            this.Controls.Add(this.loginField);
+            this.Controls.Add(this.generateBtn);
+            this.Controls.Add(this.passwField);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.passwLength);
+            this.Controls.Add(this.rockyouPassw);
+            this.Controls.Add(this.notSimilarSymb);
+            this.Controls.Add(this.specialSymb);
+            this.Name = "MainForm";
+            this.Text = "Генератор паролей";
+            ((System.ComponentModel.ISupportInitialize)(this.passwLength)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button returnAllBtn;
+        private System.Windows.Forms.Label sequrityStatus;
+        private System.Windows.Forms.TextBox specialCharsBox;
+        private System.Windows.Forms.Button saveBtn;
+        private System.Windows.Forms.TextBox loginField;
+        private System.Windows.Forms.Button generateBtn;
+        private System.Windows.Forms.TextBox passwField;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.NumericUpDown passwLength;
+        private System.Windows.Forms.CheckBox rockyouPassw;
+        private System.Windows.Forms.CheckBox notSimilarSymb;
+        private System.Windows.Forms.CheckBox specialSymb;
     }
 }
 
--- Lab_1/MainForm.cs
+++ Lab_1/MainForm.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -15,6 +16,184 @@ namespace Lab_1
         public MainForm()
         {
             InitializeComponent();
+        }
+
+        private void passwLength_ValueChanged(object sender, EventArgs e)
+        {
+            if (passwLength.Value < 10)
+            {
+                sequrityStatus.ForeColor = Color.IndianRed;
+                sequrityStatus.Text = "Ненадежный! х";
+            }
+            else if (passwLength.Value < 14)
+            {
+                sequrityStatus.ForeColor = Color.DarkOrange;
+                sequrityStatus.Text = "Средний";
+            }
+            else
+            {
+                sequrityStatus.ForeColor = Color.ForestGreen;
+                sequrityStatus.Text = "Надежный ✓";
+            }
+        }
+
+        private void generateBtn_Click(object sender, EventArgs e)
+        {
+
+            Random random = new Random();
+
+            decimal length = passwLength.Value;
+
+            string password = GeneratePassw(length, notSimilarSymb.Checked, specialSymb.Checked, rockyouPassw.Checked);
+
+            passwField.Text = password;
+
+        }
+
+        public string GeneratePassw(decimal length, bool notExcludeSimilar, bool includeSpecial, bool includeRockyou)
+        {
+            string validChars = "abcdefghjkmnpqrstuvwxyzABCDEFGHJKMNPQRSTUVWXYZ23456789";
+            string similarChars1 = "il1L";
+            string similarChars2 = "oO0";
+            string specialChars = specialCharsBox.Text;
+
+            Random random = new Random();
+
+
+            /*int randomIndex1 = random.Next(similarChars1.Length - 1); 
+
+            char randomChar1 = similarChars1[randomIndex1]; 
+
+            int randomIndex2 = random.Next(similarChars2.Length - 1); 
+
+            char randomChar2 = similarChars2[randomIndex2]; */
+
+            if (notExcludeSimilar == false)
+            {
+                validChars += similarChars1 + similarChars2;
+            }
+
+
+            if (includeSpecial)
+            {
+                validChars += specialChars;
+            }
+
+            string password = GenerateRandomPassword(length, validChars);
+
+
+            if (!includeRockyou)
+            {
+                return password;
+            }
+            else
+            {
+                if (IsPasswUnique(password))
+                {
+                    return password;
+                }
+                else
+                {
+                    while (!IsPasswUnique(password))
+                    {
+                        password = GenerateRandomPassword(length, validChars);
+                    }
+
+                    return password;
+                }
+            }
+
+        }
+
+        public static string GenerateRandomPassword(decimal length, string chars)
+        {
+            Random random = new Random();
+
+            StringBuilder password = new StringBuilder();
+
+            for (decimal i = 0; i < length; i++)
+            {
+                int randomIndex = random.Next(chars.Length);
+                char randomChar = chars[randomIndex];
+                password.Append(randomChar);
+            }
+
+            return password.ToString();
+        }
+
+        private bool IsPasswUnique(string password)
+        {
+            if (File.Exists("rockyou.txt"))
+            {
+                string[] passws = File.ReadAllLines("rockyou.txt");
+
+                foreach (string passw in passws)
+                {
+                    if (passw == password)
+                    {
+                        return false;
+                    }
+                }
+            }
+
+            return true;
+        }
+
+        private bool IsLoginUnique(string login)
+        {
+            if (File.Exists("logins.txt"))
+            {
+                using (StreamReader sr = new StreamReader("logins.txt"))
+                {
+                    string line;
+                    while ((line = sr.ReadLine()) != null)
+                    {
+                        string[] parts = line.Split(',');
+                        if (parts.Length >= 1 && parts[0] == login)
+                        {
+                            return false;
+                        }
+                    }
+                }
+            }
+
+            return true;
+        }
+
+        private void returnAllBtn_Click(object sender, EventArgs e)
+        {
+            specialCharsBox.Text = "{}[]()/\\'\"`~,:.<>";
+        }
+
+        private void saveBtn_Click(object sender, EventArgs e)
+        {
+            string login = loginField.Text;
+
+            string password = passwField.Text;
+
+            if (string.IsNullOrEmpty(login) || string.IsNullOrEmpty(password) || password == "Пароль" || login == "Логин")
+            {
+                MessageBox.Show("Поля логина и пароля пустые, проверьте введенные данные и попробуйте заново", "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
+            else if (!IsLoginUnique(login))
+            {
+                MessageBox.Show("Логин уже существует. Пожалуйста, выберите другой логин.", "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
+            using (StreamWriter sw = new StreamWriter("logins.txt", true))
+            {
+                sw.WriteLine(login + "," + password);
+                MessageBox.Show("Логин и пароль успешно сохранены.", "Инфо", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+        }
+
+        private void loginField_Enter(object sender, EventArgs e)
+        {
+            loginField.Text = string.Empty;
+            loginField.ForeColor = Color.Black;
         }
     }
 }
